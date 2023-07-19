83bbac14927dd858b92332cc235e0992542ecfde
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-19 21:55:26 +0330

    [edit] edit main.go

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
 
-//main func for running main function...
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+// main func for running main function...
 func main() {
 	fmt.Println("hello, world!")
+
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "13781378",
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	//get a database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("connected")
+
 }
