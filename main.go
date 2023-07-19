2d27ea7a110f695ecfcf1ca7607677f129e51905
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-19 22:06:37 +0330

    [add] add albomById function to app'

--- main.go
+++ main.go
@@ -52,6 +52,21 @@ func main() {
 
 }
 
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+	// An album to hold data from the returned row.
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
+}
+
 // albumsByArtist queries for albums that have the specified artist name.
 func albumsByArtist(name string) ([]Album, error) {
 	// An albums slice to hold data from returned rows.
