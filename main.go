2934d1e33bd422b15cf776dadc62de55e8dafd41
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-19 22:10:17 +0330

    [add] add addAlbum function to app to insert data

--- main.go
+++ main.go
@@ -55,6 +55,16 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Album found: %v\n", alb)
+
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // addAlbum adds the specified album to the database,
